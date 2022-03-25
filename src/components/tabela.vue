d1de576cc8ddb102fc09f3d399fdf0ce6b9013ee
Jompa14 <joao.buchholz@gmail.com>
2022-03-25 17:44:10 -0300

    teste build

---
+++ src/components/tabela.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div class="">
+        <img :src="{{mdiAlienOutline}}" alt="alienzinho">
+        <p>{{ teste }}</p>
+    </div>
+</template>
+
+<script>
+import { mdiAlienOutline } from '@mdi/js';
+export default {
+    data() {
+        mdiAlienOutline,
+        teste: 'oizinho',
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
