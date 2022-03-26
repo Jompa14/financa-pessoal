8f7ee8bdb10c0f3f017f749eea554579b3c5ddec
Jompa14 <joao.buchholz@gmail.com>
2022-03-26 13:49:57 -0300

    cria componente painel

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
     <div id="app">
-        <h1>Finança pessoal</h1>
+        <div class="navbar alinhamento">
+            <h1>Finança pessoal</h1>
+        </div>
         <tabela-usuario></tabela-usuario>
+        <painel-resumo></painel-resumo>
     </div>
 </template>
 
 <script>
 import tabelaUsuario from './components/tabela.vue'
+import painelResumo from './components/painelResumo.vue'
 export default {
   name: 'App',
   components: {
       tabelaUsuario,
+      painelResumo,
   }
 };
 </script>
-
-<style scoped>
+<!-- unico css da aplicação q tem css global -->
+<style lang="scss">
     #app {
         font-family: sans-serif;
         color: black;
     }
-    h1 {
-        color: #3ec8b3;
+    .alinhamento {
+        width: 80%;
+        max-width: 1800px;
+        margin: 0 auto;
+        @media(max-width: 600px){
+            width: 90%;
+        }
+    }
+    .navbar {
+        display: flex;
+        align-items: center;
+        h1, h2, h3 {
+            color: #3ec8b3;
+        }
+        // h2 {
+        //     color: #3ec8b3;
+        // }
     }
 </style>
---
+++ src/components/painelResumo.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div class="alinhamento">
+        <p>hello world</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // name: 'painel-resumo',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
